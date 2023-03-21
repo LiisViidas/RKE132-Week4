02f1c1b25d882567a36cb0465b320a364ca5d162
Liis VT <liis.viidas@tktk.ee>
2023-03-21 17:40:18 +0200

    create new app

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿//juhuslik number kolm korda
+
+Random rnd = new Random();
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11); //see tahendab genereeritakse 0-10
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total> {randomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿//while loop
+
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; // bpp; -true/false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+  
+    Console.WriteLine("Make a quess. Enter a number 1-3");
+    int usernumber = Int32.Parse(Console.ReadLine());
+
+    if (usernumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, You won!");
+        loopActive = false;// voi break;
+    }
+    else
+    {
+        Console.WriteLine("Oops. Try again");
+    }
+}
+
+Console.WriteLine("Have a nice day");
+
